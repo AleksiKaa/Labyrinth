22727afbe056b8259a46c43a5e3b8efbadc45305
Aleksi <aleksi.kaariainen@aalto.fi>
2021-03-05 14:23:56 +0200

    Rough implementation of Tile

---
+++ src/main/scala/Tile.scala
@@ -0,0 +1,29 @@
+sealed trait Tile {
+
+  def isUnpassable: Boolean
+
+  def containsPlayer: Boolean
+
+}
+
+object Wall extends Tile {
+
+  def isUnpassable = true
+
+  def containsPlayer: Boolean = false
+
+}
+
+class Path extends Tile {
+
+  var conPlayer = false
+
+  def isUnpassable = false
+
+  def containsPlayer = conPlayer
+
+}
+
+class Bridge extends Path
+
+
